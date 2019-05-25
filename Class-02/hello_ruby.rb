5137b5744affe6bde660a5c19068845ff2282300
mahichy <43366576+mahichy@users.noreply.github.com>
2019-05-25 17:05:34 +0600

    runing ruby
    this is my first class of ruby.
    its really fun the teacher is awesome of this course.

---
+++ Class-02/hello_ruby.rb
@@ -0,0 +1,7 @@
+comment =  "ruby is fun"
+puts comment.upcase
+
+3.times do 
+puts comment.upcase
+end
+puts Time.new
---
+++ Class-02/studio_game.rb
@@ -0,0 +1,5 @@
+gretting = "Welcome"
+3.times do 
+puts gretting.upcase
+end
+puts Time.new
